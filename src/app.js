66de876cf1681cb1420c96917daa1f8877303482
Bethly9996 <bethlyc@kabarak.ac.ke>
2022-08-04 06:07:24 +0300

    components and classes

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import './app.css';
+import Products from './Components/Products';
+
+function App() {
+
+  return (<div className='row'>
+    <div className='col-lg-8 mx-auto'>
+    <Products/>
+    </div>
+  </div>);
+
+}
+
+export default App;
